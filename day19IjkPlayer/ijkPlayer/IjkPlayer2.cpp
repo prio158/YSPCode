265b03af5ae99d674d68a152cb39ba2a5a61a4fe
czr <czr@trip.com>
2023-11-12 09:28:26 +0800

    feat:[1.3] ijkPlayer

--- day19IjkPlayer/ijkPlayer/IjkPlayer2.cpp
+++ day19IjkPlayer/ijkPlayer/IjkPlayer2.cpp
@@ -1,2 +1,93 @@
 #include "../include/IjkPlayer2.h"
+#include "IjkPlayer2.h"
 
+IjkPlayer2::IjkPlayer2()
+{
+}
+
+IjkPlayer2::~IjkPlayer2()
+{
+}
+
+int IjkPlayer2::ijkmp_create(std::function<int(void *)> msg_loop)
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_destory()
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_set_data_source(const char *url)
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_prepare_async()
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_start()
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_stop()
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_pause()
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_seek_to(long msec)
+{
+    return 0;
+}
+
+int IjkPlayer2::ijkmp_get_state()
+{
+    return 0;
+}
+
+bool IjkPlayer2::ijkmp_is_playing()
+{
+    return false;
+}
+
+long IjkPlayer2::ijkmp_get_current_position()
+{
+    return 0;
+}
+
+long IjkPlayer2::ijkmp_get_duration()
+{
+    return 0;
+}
+
+long IjkPlayer2::ijkmp_get_playable_duration()
+{
+    return 0;
+}
+
+void IjkPlayer2::ijkmp_set_loop(int loop)
+{
+}
+
+int IjkPlayer2::ijkmp_get_msg(AVMessage *msg, int block)
+{
+    return 0;
+}
+
+void IjkPlayer2::ijkmp_set_playback_volume(float volume)
+{
+}
+
+int IjkPlayer2::ijkmp_msg_loop(void *arg)
+{
+    return 0;
+}
--- day19IjkPlayer/include/IjkPlayer2.h
+++ day19IjkPlayer/include/IjkPlayer2.h
@@ -6,37 +6,57 @@
 #include <functional>
 #include "../include/ff_ffplayer.h"
 
-
 class IjkPlayer2
 {
 private:
+    /* 互斥量 */
     std::mutex mutex_;
-    FFPlayer* ffplayer;
-    std::function<int(void*)> msg_loop_;
-    std::thread* msg_thread_;
-    char* data_source_; //url
+    /* 真正的播放器 */
+    FFPlayer *ffplayer = nullptr;
+    /* 消息循环函数 */
+    std::function<int(void *)> msg_loop_;
+    /* 消息线程 */
+    std::thread *msg_thread_;
+    /* 播放的url */
+    char *data_source_;
+    /* 播放的状态：prepaered\resumed\error\completed */
     int mp_state_;
 
-
 public:
-    IjkPlayer2(/* args */);
+    IjkPlayer2();
     ~IjkPlayer2();
-    
-
-
-
+    int ijkmp_create(std::function<int(void *)> msg_loop);
+    int ijkmp_destory();
+    /* 设置要播放的url */
+    int ijkmp_set_data_source(const char *url);
+    /* 准备播放 */
+    int ijkmp_prepare_async();
+    /* 触发播放 */
+    int ijkmp_start();
+    /* 停止 */
+    int ijkmp_stop();
+    /* 暂停 */
+    int ijkmp_pause();
+    /* seek */
+    int ijkmp_seek_to(long msec);
+    /* 获取播放状态 */
+    int ijkmp_get_state();
+    /* 判断是否在播放中 */
+    bool ijkmp_is_playing();
+    /* 获取当前的播放位置 */
+    long ijkmp_get_current_position();
+    /* 总长度 */
+    long ijkmp_get_duration();
+    /* 已经播放的长度 */
+    long ijkmp_get_playable_duration();
+    /* 设置循环播放 */
+    void ijkmp_set_loop(int loop);
+    /* 读取消息 */
+    int ijkmp_get_msg(AVMessage *msg, int block);
+    /* 设置音量 */
+    void ijkmp_set_playback_volume(float volume);
+    /* loop */
+    int ijkmp_msg_loop(void *arg);
 };
 
-IjkPlayer2::IjkPlayer2(/* args */)
-{
-}
-
-IjkPlayer2::~IjkPlayer2()
-{
-}
-
-
-
-
-
-#endif 
+#endif
